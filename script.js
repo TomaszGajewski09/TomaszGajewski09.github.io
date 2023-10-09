c32c7e67ab8559117af10b1a19560145e2d4651f
TomaszGajewski09 <tomasz.gajewski0922@gmail.com>
2023-10-09 17:15:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
     });
   }*/
   
-  /*function handleDatatableRender(taskData, boardsData) {
+  function handleDatatableRender(taskData, boardsData) {
   console.log('Received task data:', taskData);
   console.log('Received boards data:', boardsData);
 
@@ -85,35 +85,8 @@ $(document).ready(function() {
     });
 
     $datatableRowEl.appendTo($tasksContainer);
-  });
-  }*/
-  
-  function handleBoardNameSelect(event) {
-  var $changedSelectEl = $(event.target);
-  var selectedBoardId = $changedSelectEl.val();
-  var $listNameSelectEl = $changedSelectEl.siblings('[data-list-name-select]');
-
-  // Clear previous options in list-name select
-  $listNameSelectEl.empty();
-
-  // Get the lists for the selected board
-  var selectedBoard = availableBoards[selectedBoardId];
-  if (selectedBoard) {
-    var lists = selectedBoard.lists;
-
-    // Populate the list-name select with options
-    lists.forEach(function (list) {
-      $listNameSelectEl.append($('<option>')
-        .addClass('crud-select__option')
-        .val(list.id)
-        .text(list.name || 'Unknown list'));
-    });
-  } else {
-    console.error('Selected board not found:', selectedBoardId);
-  }
-  }
-
-
+	});
+	}
 
 
   function getAllTasks() {
@@ -209,7 +182,7 @@ $(document).ready(function() {
     parentEl.find('[data-task-name-input]').val(taskTitle);
     parentEl.find('[data-task-content-input]').val(taskContent);
   }
-
+/*
   function handleBoardNameSelect(event) {
     var $changedSelectEl = $(event.target);
     var selectedBoardId = $changedSelectEl.val();
@@ -218,6 +191,30 @@ $(document).ready(function() {
 
     $listNameSelectEl.empty().append(preparedListOptions);
   }
+*/
+
+	function handleBoardNameSelect(event) {
+	var $changedSelectEl = $(event.target);
+	var selectedBoardId = $changedSelectEl.val();
+	var $listNameSelectEl = $changedSelectEl.siblings('[data-list-name-select]');
+
+	  $listNameSelectEl.empty();
+
+	  var selectedBoard = availableBoards[selectedBoardId];
+	  if (selectedBoard) {
+		var lists = selectedBoard.lists;
+
+		lists.forEach(function (list) {
+		  $listNameSelectEl.append($('<option>')
+			.addClass('crud-select__option')
+			.val(list.id)
+			.text(list.name || 'Unknown list'));
+		});
+	  } else {
+		console.error('Selected board not found:', selectedBoardId);
+	  }
+	}
+
 
   function handleCardCreationRequest(event) {
     var requestUrl = trelloApiRoot + '/cards';
